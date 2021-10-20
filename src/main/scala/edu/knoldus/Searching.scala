63be2e5e72e0be4411173f70c361fb89586d653a
ShashikantKnoldus <shashikant.tyagi@knoldus.com>
2021-10-20 18:33:22 +0530

    First commit

---
+++ src/main/scala/edu/knoldus/Searching.scala
@@ -0,0 +1,27 @@
+package edu.knoldus
+
+class Searching {
+
+  def binarySearch(array: Array[Int], elem: Int): Boolean ={
+    def bs_helper(arr: Array[Int], target: Int, start: Int, end: Int): Int = {
+      if (start > end) return -1
+      val mid = end - start  / 2
+      arr(mid) match {
+        case i if (i == target) => mid
+        case i if (i > target) => bs_helper(arr, target, start, mid - 1)
+        case _ => bs_helper(arr, target, mid + 1, end)
+      }
+    }
+
+    val res=bs_helper(array, elem, 0, array.length-1)
+    if (res==(-1)) false
+    else true
+  }
+
+
+  def linearSearch(array: Array[Int], elem: Int): Boolean = {
+    if (array.isEmpty) false
+    else if (array.head == elem) true
+    else linearSearch(array.tail, elem)
+  }
+}
